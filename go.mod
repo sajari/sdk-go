7828f289fe58e3ccaaa9bc18618193d52d94dd0a
Jonathan Ingram <jonathan.b.ingram@gmail.com>
2020-10-16 10:54:44 +1100

    all: add support for v4 OpenAPI
    This is done in a progressive way to allow callers to opt in to using
    the v4 API.

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.13
 
 require (
 	code.sajari.com/protogen-go v0.0.0-20200920230145-133a858bb8ab
-	github.com/golang/protobuf v1.4.0
-	google.golang.org/grpc v1.20.0
+	github.com/golang/protobuf v1.4.2
+	golang.org/x/oauth2 v0.0.0-20180821212333-d2e6202438be
+	google.golang.org/grpc v1.27.0
 )
--- opts.go
+++ opts.go
@@ -1,9 +1,28 @@
 package sajari
 
-import "google.golang.org/grpc"
+import (
+	"google.golang.org/grpc"
+
+	"code.sajari.com/sdk-go/internal/openapi"
+)
 
 // Opt is a type which defines Client options.
 type Opt func(c *Client)
+
+// WithV4Endpoint configures the client to use a v4 endpoint.
+func WithV4Endpoint(endpoint string) Opt {
+	return func(c *Client) {
+		c.v4 = true
+
+		c.openAPI.config.Servers = openapi.ServerConfigurations{
+			{
+				URL: endpoint,
+			},
+		}
+
+		c.openAPI.client = openapi.NewAPIClient(c.openAPI.config)
+	}
+}
 
 // WithEndpoint configures the client to use a custom endpoint.
 func WithEndpoint(endpoint string) Opt {
@@ -13,8 +32,17 @@ func WithEndpoint(endpoint string) Opt {
 }
 
 // WithCredentials sets the client credentials used in each request.
-func WithCredentials(c Credentials) Opt {
-	return WithGRPCDialOption(grpc.WithPerRPCCredentials(creds{c}))
+func WithCredentials(cr Credentials) Opt {
+	return func(c *Client) {
+		if kc, ok := cr.(keyCreds); ok {
+			c.openAPI.auth = openapi.BasicAuth{
+				UserName: kc.keyID,
+				Password: kc.keySecret,
+			}
+		}
+		opt := WithGRPCDialOption(grpc.WithPerRPCCredentials(creds{cr}))
+		opt(c)
+	}
 }
 
 // WithGRPCDialOption returns an Opt which appends a new grpc.DialOption
