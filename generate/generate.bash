466db89f7769f03205c4fe4399a58e2f66c31414
Jonathan Ingram <jonathan.b.ingram@gmail.com>
2020-11-17 16:18:51 +1100

    generate: initial commit of scripts

---
+++ generate/generate.bash
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+cd "$(dirname "$0")"
+
+function die() {
+  echo 1>&2 $*
+  exit 1
+}
+
+if [ -z "$GEN_PATH" ]; then
+  die "GEN_PATH must be set (e.g. path to generated directory)"
+fi
+
+docker-entrypoint.sh generate \
+    -i /openapi.json \
+    -g go \
+    --git-user-id sajari \
+    --git-repo-id sdk-go \
+    --additional-properties enumClassPrefix=true \
+    -o $GEN_PATH
