ab71195b883185c6dc86eb37e285a2093c466643
Jonathan Ingram <jonathan.b.ingram@gmail.com>
2020-11-17 16:44:32 +1100

    collection: add UpdateCollection call

--- internal/openapi/api_collections.go
+++ internal/openapi/api_collections.go
@@ -676,3 +676,143 @@ func (a *CollectionsApiService) QueryCollectionExecute(r ApiQueryCollectionReque
 
 	return localVarReturnValue, localVarHTTPResponse, nil
 }
+
+type ApiUpdateCollectionRequest struct {
+	ctx               _context.Context
+	ApiService        *CollectionsApiService
+	collectionId      string
+	v4beta1Collection *V4beta1Collection
+	updateMask        *string
+}
+
+func (r ApiUpdateCollectionRequest) V4beta1Collection(v4beta1Collection V4beta1Collection) ApiUpdateCollectionRequest {
+	r.v4beta1Collection = &v4beta1Collection
+	return r
+}
+func (r ApiUpdateCollectionRequest) UpdateMask(updateMask string) ApiUpdateCollectionRequest {
+	r.updateMask = &updateMask
+	return r
+}
+
+func (r ApiUpdateCollectionRequest) Execute() (V4beta1Collection, *_nethttp.Response, error) {
+	return r.ApiService.UpdateCollectionExecute(r)
+}
+
+/*
+ * UpdateCollection Update collection
+ * Update the details of a collection.
+ * @param ctx _context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
+ * @param collectionId The collection to update, e.g. `my-collection`.
+ * @return ApiUpdateCollectionRequest
+ */
+func (a *CollectionsApiService) UpdateCollection(ctx _context.Context, collectionId string) ApiUpdateCollectionRequest {
+	return ApiUpdateCollectionRequest{
+		ApiService:   a,
+		ctx:          ctx,
+		collectionId: collectionId,
+	}
+}
+
+/*
+ * Execute executes the request
+ * @return V4beta1Collection
+ */
+func (a *CollectionsApiService) UpdateCollectionExecute(r ApiUpdateCollectionRequest) (V4beta1Collection, *_nethttp.Response, error) {
+	var (
+		localVarHTTPMethod   = _nethttp.MethodPatch
+		localVarPostBody     interface{}
+		localVarFormFileName string
+		localVarFileName     string
+		localVarFileBytes    []byte
+		localVarReturnValue  V4beta1Collection
+	)
+
+	localBasePath, err := a.client.cfg.ServerURLWithContext(r.ctx, "CollectionsApiService.UpdateCollection")
+	if err != nil {
+		return localVarReturnValue, nil, GenericOpenAPIError{error: err.Error()}
+	}
+
+	localVarPath := localBasePath + "/v4beta1/collections/{collection_id}"
+	localVarPath = strings.Replace(localVarPath, "{"+"collection_id"+"}", _neturl.PathEscape(parameterToString(r.collectionId, "")), -1)
+
+	localVarHeaderParams := make(map[string]string)
+	localVarQueryParams := _neturl.Values{}
+	localVarFormParams := _neturl.Values{}
+	if r.v4beta1Collection == nil {
+		return localVarReturnValue, nil, reportError("v4beta1Collection is required and must be specified")
+	}
+
+	if r.updateMask != nil {
+		localVarQueryParams.Add("update_mask", parameterToString(*r.updateMask, ""))
+	}
+	// to determine the Content-Type header
+	localVarHTTPContentTypes := []string{"application/json"}
+
+	// set Content-Type header
+	localVarHTTPContentType := selectHeaderContentType(localVarHTTPContentTypes)
+	if localVarHTTPContentType != "" {
+		localVarHeaderParams["Content-Type"] = localVarHTTPContentType
+	}
+
+	// to determine the Accept header
+	localVarHTTPHeaderAccepts := []string{"application/json"}
+
+	// set Accept header
+	localVarHTTPHeaderAccept := selectHeaderAccept(localVarHTTPHeaderAccepts)
+	if localVarHTTPHeaderAccept != "" {
+		localVarHeaderParams["Accept"] = localVarHTTPHeaderAccept
+	}
+	// body params
+	localVarPostBody = r.v4beta1Collection
+	req, err := a.client.prepareRequest(r.ctx, localVarPath, localVarHTTPMethod, localVarPostBody, localVarHeaderParams, localVarQueryParams, localVarFormParams, localVarFormFileName, localVarFileName, localVarFileBytes)
+	if err != nil {
+		return localVarReturnValue, nil, err
+	}
+
+	localVarHTTPResponse, err := a.client.callAPI(req)
+	if err != nil || localVarHTTPResponse == nil {
+		return localVarReturnValue, localVarHTTPResponse, err
+	}
+
+	localVarBody, err := _ioutil.ReadAll(localVarHTTPResponse.Body)
+	localVarHTTPResponse.Body.Close()
+	if err != nil {
+		return localVarReturnValue, localVarHTTPResponse, err
+	}
+
+	if localVarHTTPResponse.StatusCode >= 300 {
+		newErr := GenericOpenAPIError{
+			body:  localVarBody,
+			error: localVarHTTPResponse.Status,
+		}
+		if localVarHTTPResponse.StatusCode == 404 {
+			var v RpcStatus
+			err = a.client.decode(&v, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+			if err != nil {
+				newErr.error = err.Error()
+				return localVarReturnValue, localVarHTTPResponse, newErr
+			}
+			newErr.model = v
+			return localVarReturnValue, localVarHTTPResponse, newErr
+		}
+		var v GatewayruntimeError
+		err = a.client.decode(&v, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+		if err != nil {
+			newErr.error = err.Error()
+			return localVarReturnValue, localVarHTTPResponse, newErr
+		}
+		newErr.model = v
+		return localVarReturnValue, localVarHTTPResponse, newErr
+	}
+
+	err = a.client.decode(&localVarReturnValue, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+	if err != nil {
+		newErr := GenericOpenAPIError{
+			body:  localVarBody,
+			error: err.Error(),
+		}
+		return localVarReturnValue, localVarHTTPResponse, newErr
+	}
+
+	return localVarReturnValue, localVarHTTPResponse, nil
+}
