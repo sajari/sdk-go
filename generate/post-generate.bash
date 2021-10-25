e8068089f035ba9528282e4aee747883fdf392b9
Jonathan Ingram <jonathan.b.ingram@gmail.com>
2021-10-25 17:00:32 +1100

    internal/openapi: fix SendEventRequest.metadata generated Go type

--- generate/post-generate.bash
+++ generate/post-generate.bash
@@ -13,4 +13,9 @@ if [ -z "$GEN_PATH" ]; then
     die "GEN_PATH must be set, e.g. /path/to/sajari/sdk-go/internal/openapi"
 fi
 
-goimports -w $GEN_PATH
+# There's a bug in the generator which turns an object into the wrong Go map.
+# It's probably to do with { "additionalProperties": { "type": "object" } } but
+# either way we need to fix it in post.
+sed -i 's/map\[string\]map\[string\]interface{}/map\[string\]interface{}/g' "$GEN_PATH"/model_send_event_request.go
+
+goimports -w "$GEN_PATH"
